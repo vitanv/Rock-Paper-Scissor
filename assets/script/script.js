eeabceeaa404c4f916acbd209cb9b782573fcc1d
vitanv <vitanvlad20@gmail.com>
2022-09-07 23:23:15 +0100

    Fixed script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 
 window.onload = function(){
     console.log("Worked");
-    document.getElementById("content").innerHTML = <i class="fa-regular fa-hand-scissors fa-2xl"></i>;
+    document.getElementById("content").innerHTML = "Placeholder";
 }
