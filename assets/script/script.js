9079f9549f37e8237ca5d6c3b7d814ad2233076b
vitanv <vitanvlad20@gmail.com>
2022-09-08 23:34:23 +0100

    Added a function that reads user input

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,11 @@
+const chooseHand = (value) =>{
+    switch(value){
+        case "rock":
+            document.getElementById("Player").innerHTML = '<i class="fa-regular fa-hand-back-fist fa-2xl"></i>';
+        case "paper":
+            document.getElementById("Player").innerHTML = '<i class="fa-regular fa-hand fa-2xl"></i>';
+        case "scissors":
+            document.getElementById("Player").innerHTML = '<i class="fa-regular fa-hand-scissors fa-2xl"></i>';
 
+    }
+}
