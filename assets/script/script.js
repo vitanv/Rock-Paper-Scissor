8ba6e53059a26b0463b7241b541e6c8bc0cf0c24
vitanv <vitanvlad20@gmail.com>
2022-09-12 21:06:14 +0100

    Fixed  chooseHand bug

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,10 +2,13 @@ const chooseHand = (value) =>{
     switch(value){
         case "rock":
             document.getElementById("Player").innerHTML = '<image src="assets/style/Images/Rock.png" class="Button-Image">';
+            break;
         case "paper":
             document.getElementById("Player").innerHTML = '<image src="assets/style/Images/Paper.png" class="Button-Image">';
+            break;
         case "scissors":
             document.getElementById("Player").innerHTML = '<image src="assets/style/Images/Scissor.png" class="Button-Image">';
+            break;
 
     }
 }
