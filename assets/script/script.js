a33e14fe62486d2d8a84d54b6453a6bff0349592
vitanv <vitanvlad20@gmail.com>
2022-09-12 21:07:09 +0100

    Resized display image

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,13 @@
 const chooseHand = (value) =>{
     switch(value){
         case "rock":
-            document.getElementById("Player").innerHTML = '<image src="assets/style/Images/Rock.png" class="Button-Image">';
+            document.getElementById("Player").innerHTML = '<image src="assets/style/Images/Rock.png" class="Display-Image">';
             break;
         case "paper":
-            document.getElementById("Player").innerHTML = '<image src="assets/style/Images/Paper.png" class="Button-Image">';
+            document.getElementById("Player").innerHTML = '<image src="assets/style/Images/Paper.png" class="Display-Image">';
             break;
         case "scissors":
-            document.getElementById("Player").innerHTML = '<image src="assets/style/Images/Scissor.png" class="Button-Image">';
+            document.getElementById("Player").innerHTML = '<image src="assets/style/Images/Scissor.png" class="Display-Image">';
             break;
 
     }
