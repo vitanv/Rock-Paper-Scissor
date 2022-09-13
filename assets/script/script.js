0fe6623ae78e97e86e28161a6d844a58f0c496ed
vitanv <vitanvlad20@gmail.com>
2022-09-13 23:37:13 +0100

    Fixed button calling the wrong function

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,22 +2,28 @@ const rockImage = '<image src="assets/style/Images/Rock.png" class="Display-Imag
 const paperImage = '<image src="assets/style/Images/Paper.png" class="Display-Image">';
 const scissorImage = '<image src="assets/style/Images/Scissor.png" class="Display-Image">';
 
+let playerHand;
+let computerHand;
  
 function playGame (value){
     playerChoice(value);
     computerChoice();
+   
 }
 
 function playerChoice (value){
     switch(value){
         case "rock":
             document.getElementById("Player").innerHTML = rockImage;
+            playerHand = "rock";
             break;
         case "paper":
             document.getElementById("Player").innerHTML = paperImage;
+            playerHand = "paper";
             break;
         case "scissors":
             document.getElementById("Player").innerHTML = scissorImage;
+            playerHand = "scissors";
             break;
 
     }
@@ -28,12 +34,15 @@ function computerChoice(){
     switch(random){
         case 1:
             document.getElementById("AI").innerHTML = rockImage;
+            computerHand = "rock";
             break;
         case 2:
             document.getElementById("AI").innerHTML = paperImage;
+            computerHand = "paper";
             break;
         case 3:
             document.getElementById("AI").innerHTML = scissorImage;
+            computerHand = "scissors";
             break;
     }
 }
